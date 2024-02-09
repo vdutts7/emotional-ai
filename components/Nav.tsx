e7e47f67094374130c00ffe5496aa070adf6b9bc
vd7 <me@vd7.io>
2024-02-09 09:34:01 -0800

    initial setup

---
+++ components/Nav.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useLayoutEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Moon, Sun, Brain, Heart, Smile, Frown } from "lucide-react";
+import Github from "./logos/GitHub";
+
+const getRandomEmotionalAIIcon = () => {
+  const icons = [Brain, Heart, Smile, Frown];
+  return icons[Math.floor(Math.random() * icons.length)];
+};
+
+export const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const EmotionalAIIcon = getRandomEmotionalAIIcon();
+
+  useLayoutEffect(() => {
+    const el = document.documentElement;
+    setIsDarkMode(el.classList.contains("dark"));
+  }, []);
+
+  const toggleDark = () => {
+    const el = document.documentElement;
+    el.classList.toggle("dark");
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <div className="px-4 py-2 flex items-center h-14 z-50 bg-card border-b border-border">
+      <div>
+        <EmotionalAIIcon className="h-5 w-auto" />
+      </div>
+      <div className="ml-auto flex items-center gap-1">
+        <Button
+          onClick={() => {
+            window.open(
+              "https://github.com/vdutts7/emotional-ai",
+              "_blank",
+              "noopener noreferrer"
+            );
+          }}
+          variant="ghost"
+          className="ml-auto flex items-center gap-1.5"
+        >
+          <span>
+            <Github className="size-4" />
+          </span>
+          <span>Star on GitHub</span>
+        </Button>
+        <Button
+          onClick={toggleDark}
+          variant="ghost"
+          className="ml-auto flex items-center gap-1.5"
+        >
+          <span>
+            {isDarkMode ? (
+              <Sun className="size-4" />
+            ) : (
+              <Moon className="size-4" />
+            )}
+          </span>
+          <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
